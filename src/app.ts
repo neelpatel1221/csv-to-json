a3a3ef77578df3b0458198fa3365b2c11051c263
Neel Patel <pneel014@gmail.com>
2025-05-20 22:54:00 +0530

    Refactor app configuration to use environment variables for port and improve logging

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express  from "express";
 import { createUsersTable } from "./config/db";
 import csvToJsonRouter from "./routes/csvToJsonConverterRouter"
+import dotenv from "dotenv";
+dotenv.config()
 const app = express()
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use("/", csvToJsonRouter);
 
 // Function to create the users Table if exists
 createUsersTable()
-app.listen(3000, ()=>{
-    console.log("Server Started on Port 3000");
+app.listen(PORT, ()=>{
+    console.log(`Server Started on Port ${PORT}`);
     
 })
